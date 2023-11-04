db9edddb06fad78bad66de0f0bbd033bc640996c
Eryniox <Eryniox@users.noreply.github.com>
2023-11-04 16:08:14 +0100

    Example Scheduled task

--- Update-FFSGit.ps1
+++ Update-FFSGit.ps1
@@ -5,6 +5,11 @@ param(
     [string] $WorkingDir
 )
 
+#Example Scheduled task: 
+#<Command>C:\WINDOWS\system32\WindowsPowerShell\v1.0\powershell.exe</Command>
+#<Arguments>-ExecutionPolicy Bypass -File "C:\Scripts\PS-Sync-Files-FTPS\Update-FFSGit.ps1" -WorkingDir "C:\Scripts\PS-Sync-Files-FTPS"
+#<WorkingDirectory>C:\Scripts</WorkingDirectory>
+
 #cd "C:\plex\Scripts\PS-Sync-Files-FTPS"
 Set-Location -Path $WorkingDir
 git fetch
